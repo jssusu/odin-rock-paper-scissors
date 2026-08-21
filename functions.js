7c8d32431a389cefef4dee1a0f97dd03c5f02e56
jssusu <jeffrey.su30@gmail.com>
2026-08-21 10:04:43 -0400

    Implement functions determining the round's winner and playing a round

--- functions.js
+++ functions.js
@@ -1,26 +1,63 @@
 let computerScore = 0, humanScore = 0;
+let roundCount = 1;
 const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    return choices[Math.floor(Math.random() * 3)]
+    return choices[Math.floor(Math.random() * 3)];
+}
+
+function capitalize(string) {
+    return string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
 }
 
 function getHumanChoice() {
     let humanChoice;
     do {
-        humanChoice = prompt("Rock, Paper, or Scissors?").trim().toLowerCase();
+        humanChoice = capitalize(prompt("Rock, Paper, or Scissors?").trim());
         if (!choices.includes(humanChoice)) {
-            alert("Invalid choice.");
+            alert("Invalid choice. Enter \"Rock\", \"Paper\", or \"Scissors\". This is not case-sensitive.");
         }
     } while (choices.includes(humanChoice));
 
     return humanChoice;
 }
 
-function determineWinner(computerChoice, humanChoice) {
+// Returns true if the computer wins, false if the human wins, or null if there is a tie.
+function computerWins(computerChoice, humanChoice) {
+    if (computerChoice === humanChoice) {
+        return null;
+    }
 
+    switch (computerChoice) {
+        case "Rock":
+            return humanChoice === "Scissors" ? true : false;
+        case "Paper":
+            return humanChoice === "Rock" ? true : false;
+        case "Scissors":
+            return humanChoice === "Paper" ? true : false;
+    }
 }
 
 function playRound() {
+    let computerChoice = getComputerChoice(), humanChoice = getHumanChoice();
+    let computerWon = computerWins(computerChoice, humanChoice);
 
+    console.log(`Round ${roundCount}:`);
+
+    switch (computerWon) {
+        case null:
+            console.log("It's a tie!");
+            break;
+        case true: 
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}!`);
+            computerScore++;
+            break;
+        case false:
+            console.log(`You win! ${humanChoice} beats ${computerChoice}!`);
+            humanScore++;
+            break;
+    }
+
+    console.log(`Current Score (You - Computer): ${humanScore} - ${computerScore}`);
+    roundCount++;
 }
